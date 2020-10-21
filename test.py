af7bc37359eec4fb5e5a48a1c91a7b949d05cbbc
Filiberto Asare-Akuffo <fa15c@my.fsu.edu>
2020-10-20 23:19:02 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+Python 2.7.16 (v2.7.16:413a49145e, Mar  4 2019, 01:30:55) [MSC v.1500 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> First_name = "Filiberto"
+>>> Last_name = "Asare-Akuffo"
+>>> fullname = First_name + Last_name
+>>> fullname
+'FilibertoAsare-Akuffo'
+>>> 
